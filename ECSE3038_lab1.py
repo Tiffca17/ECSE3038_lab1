efec7cf85513e7f71bab916f8d25eb5ac99e489c
Tiffca17 <92198529+Tiffca17@users.noreply.github.com>
2024-02-01 21:22:00 -0500

    Second function

--- ECSE3038_lab1.py
+++ ECSE3038_lab1.py
@@ -4,3 +4,14 @@
 def parallel(*resistances):
     totalres = 1 / sum((1 / resistance) for resistance in resistances)
     print("The total resistance is",totalres)
+
+#Write a function, potential_divider(), that takes two values as argument, a number that represents a voltage 
+#supply value and a list of numbers that represent resistance values of resistors connected in series. 
+#The function should output the voltage drop across each resistor in your resistor list. 
+#The function should be able to accept a list of numbers of any length.
+    
+def potential_divider(voltage,*res):
+    volt_drops = [voltage * resistance / sum(res) for resistance in res]
+    print("The voltage drops are", volt_drops)
+
+potential_divider(5,100,20,50)
